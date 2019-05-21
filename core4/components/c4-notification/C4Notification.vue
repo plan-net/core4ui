e2eac31c46abca7cc7ef8722ad8dc8f31cb7e36c
oto <o.todorashko@plan-net.com>
2019-05-21 14:50:50 +0200

    new component c4-notification

---
+++ core4/components/c4-notification/C4Notification.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-alert :value="show" :type="type" :transition="transition">
+    {{message}}
+    <slot></slot>
+  </v-alert>
+</template>
+
+<script>
+export default {
+  name: "C4Notification",
+  props: {
+    /**
+     * Show/hide notification
+     */
+    show: {
+      type: [Boolean],
+      required: false,
+      default: false
+    },
+    /**
+     * Type of notifications. Possible types are: success, info, warning and error
+     */
+    type: {
+      type: [String],
+      required: false,
+      default: 'error'
+    },
+    /**
+     * Notification message
+     */
+    message: {
+      type: [String],
+      required: false,
+      default: ''
+    },
+    /**
+     * Transition type
+     */
+    transition: {
+      type: [String],
+      required: false,
+      default: 'slide-y-transition'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
