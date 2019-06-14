ae0619ed8a3d32ad7ea79aeb4d0b2c2daa5925fa
oto <o.todorashko@plan-net.com>
2019-06-14 13:13:45 +0200

    [oto.CORE4-290]:fix: add close icon, time-out handler, remove context

--- core4/components/c4-notification/C4Notification.md
+++ core4/components/c4-notification/C4Notification.md
@@ -6,14 +6,25 @@ Component for displaying notifications
 ##### Example: Notification with message
 
 ```jsx
-<c4-notification :show="<true | false>"  :type="<success | info | warning | error>" :message="<some text>">
+<c4-notification :name="<str>"
+                 :show="<true | false>"
+                 :type="<success | info | warning | error>"
+                 :message="<some text>"
+                 :dismissible="<true | false>"
+                 :timeout="<int>"
+                 @timeout-handler="<function callback>">
 </c4-notification>
 ```
 
 ##### Example: Notification with component slot
 
 ```jsx
-<c4-notification :show="<true | false>"  :type="<success | info | warning | error>">
+<c4-notification :name="<str>"
+                 :show="<true | false>"
+                 :type="<success | info | warning | error>"
+                 :dismissible="<true | false>"
+                 :timeout="<int>"
+                 @timeout-handler="<function callback>">
     <component :is="socket-reconnect-error"></component>
 </c4-notification>
 ```
@@ -21,7 +32,13 @@ Component for displaying notifications
 ##### Example: Notification with message and component slot
 
 ```jsx
-<c4-notification :show="<true | false>"  :type="<success | info | warning | error>" :message="<some txt>">
+<c4-notification :name="<str>"
+                 :show="<true | false>"
+                 :type="<success | info | warning | error>"
+                 :message="<some text>"
+                 :dismissible="<true | false>"
+                 :timeout="<int>"
+                 @timeout-handler="<function callback>">
     <component :is="socket-reconnect-error"></component>
 </c4-notification>
 ```
--- core4/components/c4-notification/C4Notification.vue
+++ core4/components/c4-notification/C4Notification.vue
@@ -1,48 +1,103 @@
 <template>
-  <v-alert :value="show" :type="type" :transition="transition">
+  <v-alert :value="show" :type="type" :transition="transition" :dismissible="dismissible">
     {{message}}
     <slot></slot>
   </v-alert>
 </template>
 
 <script>
-export default {
-  name: "C4Notification",
-  props: {
-    /**
-     * Show/hide notification
-     */
-    show: {
-      type: [Boolean],
-      required: false,
-      default: false
+  export default {
+    name: 'notification',
+    props: {
+      method: {
+        // @timeout-handler
+        type: Function
+      },
+      /**
+       * Show/hide notification.
+       */
+      show: {
+        type: [Boolean],
+        required: false,
+        default: false
+      },
+      /**
+       * Type of notifications. Possible types are: success, info, warning and error.
+       */
+      type: {
+        type: [String],
+        required: false,
+        default: 'error'
+      },
+      /**
+       * Notification message.
+       */
+      message: {
+        type: [String],
+        required: false,
+        default: ''
+      },
+      /**
+       * Sets the component transition.
+       * Can be one of the built in transitions or your own.
+       * Build in transitions:
+       *    - slide-x-transition
+       *    - slide-y-transition
+       *    - scale-transition
+       *    - fade-transition
+       *    - expand-transition
+       *
+       */
+      transition: {
+        type: [String],
+        required: false,
+        default: 'slide-y-transition'
+      },
+      /**
+       * Show/hide close icon.
+       */
+      dismissible: {
+        type: [Boolean],
+        required: false,
+        default: false
+      },
+      /**
+       * Time (in milliseconds) to wait until snackbar is automatically hidden.
+       * Use 0 to keep open indefinitely.
+       */
+      timeout: {
+        type: [Number],
+        required: false,
+        default: 0
+      },
+      /**
+       *  ToDo: add description
+       */
+      name: {
+        type: [String], // socket_reconnect_error
+        required: true
+      }
     },
-    /**
-     * Type of notifications. Possible types are: success, info, warning and error
-     */
-    type: {
-      type: [String],
-      required: false,
-      default: 'error'
+    data () {
+      return {
+        timerId: null
+      }
     },
-    /**
-     * Notification message
-     */
-    message: {
-      type: [String],
-      required: false,
-      default: ''
+    mounted () {
+      this.$nextTick(() => {
+        // element has definitely been added to the DOM
+
+        if (this.timeout) {
+          this.timerId = setTimeout(() => {
+            this.$emit('timeout-handler', this.name, { state: false })
+          }, this.timeout)
+        }
+      })
     },
-    /**
-     * Transition type
-     */
-    transition: {
-      type: [String],
-      required: false,
-      default: 'slide-y-transition'
+    beforeDestroy () {
+      clearTimeout(this.timerId)
     }
   }
-}
 </script>
 
 <style scoped>
