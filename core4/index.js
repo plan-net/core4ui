ac155c02255a2843546c05b7bb3dd09b79393c7a
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2019-07-04 15:21:25 +0200

    Added Daterange component (Weekpicker)

--- core4/index.js
+++ core4/index.js
@@ -4,20 +4,22 @@ import C4Card from './components/c4-card/C4Card'
 import C4Dialog from './components/c4-dialog/C4Dialog'
 import C4Numbers from './components/c4-numbers/C4Numbers'
 import C4Notification from './components/c4-notification/C4Notification'
-import {setRoutes} from './internal/routes/index.js'
-import {setAjaxConfig} from './internal/axios.config.js'
-import {setCookieConfig} from './internal/cookie.service.js'
-import {setStore} from './store'
+import { setRoutes } from './internal/routes/index.js'
+import { setAjaxConfig } from './internal/axios.config.js'
+import { setCookieConfig } from './internal/cookie.service.js'
+import { setStore } from './store'
 import C4DataTable from './components/c4-table/C4Table'
+import C4DateRange from './components/c4-daterange/C4DateRange'
 // import C4DataTablePlus from './components/c4-table-plus/C4TablePlus'
 import C4Empty from './components/c4-empty/C4Empty'
 import bus from './event-bus'
 import helper from './helper'
 import './plugins/vee-validate'
-import VeeValidate, {Validator} from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
 import en from 'vee-validate/dist/locale/en'
 import Router from 'vue-router'
 import Vuetify from 'vuetify'
+import moment from 'moment'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 import 'vuetify/dist/vuetify.min.css'
@@ -29,7 +31,7 @@ import './styles/theme-light.scss'
 import numbro from 'numbro'
 import deDE from 'numbro/languages/de-DE.js'
 
-import {i18n, veeValidateDictionary} from './translations'
+import { i18n, veeValidateDictionary } from './translations'
 // Vee-Validator
 // app wide styles, fonts
 
@@ -37,13 +39,13 @@ numbro.registerLanguage(deDE)
 numbro.setLanguage(deDE.languageTag)
 
 const install = (Vue, options) => {
+  moment.locale('de');
   Vue.prototype.i18n = i18n
   Vue.prototype.$bus = bus
   Vue.prototype.$helper = helper
   Vue.prototype.$store = options.store
   Vue.prototype.$numbro = numbro
   /// /////////////////
-
   Vue.use(Router)
   Vue.use(VeeValidate, {
     aria: false,
@@ -68,6 +70,7 @@ const install = (Vue, options) => {
   Vue.component('c4-datatable', C4DataTable)
   Vue.component('c4-webapp', C4Webapp)
   Vue.component('c4-empty', C4Empty)
+  Vue.component('c4-daterange', C4DateRange)
 
   Vue.use(Vuetify, {
     theme: options.config.THEME,
@@ -83,4 +86,4 @@ export default {
   install
 }
 
-export {C4Webapp}
+export { C4Webapp }
