a037f294d8baff7bff6062943cf2b6c315739c5c
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2019-08-02 09:25:11 +0200

    update to styleguide

---
+++ src/components/design/Design.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <img
+      src="./n1.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./n2.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./n3.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./n4.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./n5.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./n6.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./m1.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./m2.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./m3.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./m4.png"
+      alt=""
+    >
+    <hr>
+    <img
+      src="./m5.png"
+      alt=""
+    >
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Design'
+}
+</script>
+
+<style lang="css" scoped>
+img {
+  width: 85%;
+  max-width: 1024px;
+  height: auto;
+}
+</style>
+
+<docs>
+### Description
+
+These pictures serve as a rough guide for the Look and Feel of CORE4OS Webapplications.
+
+```jsx
+<design />
+```
+</docs>
