25a1f1c58677669710144eca9bf20c6ce3a9a4ce
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2020-04-17 15:10:54 +0200

    ad version node-script for injecting version during buildtime and avoiding additional api calls

--- core4/store/state.js
+++ core4/store/state.js
@@ -1,233 +1,230 @@
-import Auth from '../Auth'
-import bus from '../event-bus.js'
-import router from '../internal/routes/index.js'
-import { axiosInternal } from '../internal/axios.config.js'
-import { getVuetify } from '../plugins/vuetify'
-export function inIframe () {
+import Auth from "../Auth";
+import bus from "../event-bus.js";
+import router from "../internal/routes/index.js";
+import { axiosInternal } from "../internal/axios.config.js";
+import { getVuetify } from "../plugins/vuetify";
+export function inIframe() {
   try {
-    return window.self !== window.top
+    return window.self !== window.top;
   } catch (e) {
-    return true
+    return true;
   }
 }
 const state = {
   hasOwnTheme: false,
   loading: false,
-  title: 'CORE4OS',
+  title: "CORE4OS",
   dark: false,
   inWidget: false,
   appBarVisible: true,
   menu: [],
-  version: '',
-  contact: '@',
+  version: "",
+  contact: "@",
   profile: {
     error: null,
-    authenticated: false
-  }
-}
+    authenticated: false,
+  },
+};
 
 const actions = {
-  showNotification ({ commit }, payload) {
-    bus.$emit('SHOW_NOTIFICATION', payload)
+  showNotification({ commit }, payload) {
+    bus.$emit("SHOW_NOTIFICATION", payload);
   },
-  async fetchProfile ({ commit, dispatch, state }) {
-    const profile = await Auth.profile()
+  async fetchProfile({ commit, dispatch, state }) {
+    const profile = await Auth.profile();
     const dto = {
       authenticated: true,
       name: profile.name,
       realname: profile.realname,
       email: profile.email,
       perm: profile.perm,
-      role: profile.role
-    }
-    commit('set_profile', dto)
+      role: profile.role,
+    };
+    commit("set_profile", dto);
   },
-  async fetchSettings ({ commit, dispatch, state }) {
-    const setting = await Auth.setting()
-    commit('set_menu', setting.data.menu)
-    commit('set_profile', { authenticated: true })
+  async fetchSettings({ commit, dispatch, state }) {
+    const setting = await Auth.setting();
+    commit("set_menu", setting.data.menu);
+    commit("set_profile", { authenticated: true });
     if (state.hasOwnTheme === false) {
-      commit('set_dark', setting.data.dark)
+      commit("set_dark", setting.data.dark);
     }
-    commit('set_contact', setting.data.contact)
-    if (router.instance.history.current.name === 'login') {
-      dispatch('gotoStart')
+    commit("set_contact", setting.data.contact);
+    if (router.instance.history.current.name === "login") {
+      dispatch("gotoStart");
     }
-    const info = await Auth.version()
-    commit('set_version', info.version)
-    return true
+    const info = await Auth.version();
+    commit("set_version", info.version);
+    return true;
   },
-  async gotoStart ({ commit, dispatch }) {
-    router.instance.push('/')
-    commit('set_profile', { authenticated: true })
-    await dispatch('fetchSettings')
+  async gotoStart({ commit, dispatch }) {
+    router.instance.push("/");
+    commit("set_profile", { authenticated: true });
+    await dispatch("fetchSettings");
   },
-  gotoLogin ({ commit }) {
-    window.localStorage.clear()
-    commit('clear_profile')
+  gotoLogin({ commit }) {
+    window.localStorage.clear();
+    commit("clear_profile");
     window.setTimeout(() => {
-      window.location.assign('/core4/api/v1/login')
-    }, 200)
+      window.location.assign("/core4/api/v1/login");
+    }, 200);
   },
-  logout ({ dispatch }) {
+  logout({ dispatch }) {
     Auth.logout()
       .then(function () {
-        dispatch('gotoLogin')
+        dispatch("gotoLogin");
       })
-      .catch(err => {
-        dispatch('gotoLogin')
-        return Promise.reject(err)
-      })
+      .catch((err) => {
+        dispatch("gotoLogin");
+        return Promise.reject(err);
+      });
   },
-  setLoading ({ commit }, payload) {
-    commit('set_loading', payload)
+  setLoading({ commit }, payload) {
+    commit("set_loading", payload);
   },
-  setTitle ({ commit }, payload) {
-    commit('set_title', payload)
+  setTitle({ commit }, payload) {
+    commit("set_title", payload);
   },
-  initializeApp ({ commit, dispatch }, payload) {
-    dispatch('setTitle', payload.TITLE)
+  initializeApp({ commit, dispatch }, payload) {
+    dispatch("setTitle", payload.TITLE);
     if (payload.DARK != null) {
-      commit('set_dark', payload.DARK)
-      state.hasOwnTheme = true // do not show theme switch
+      commit("set_dark", payload.DARK);
+      state.hasOwnTheme = true; // do not show theme switch
     }
   },
-  setWidgetTitle ({ commit, dispatch }, payload) {
-    commit('set_title', payload)
+  setWidgetTitle({ commit, dispatch }, payload) {
+    commit("set_title", payload);
   },
-  resetWidgetTitle ({ commit, dispatch }, payload) {
-    commit('set_title', payload)
+  resetWidgetTitle({ commit, dispatch }, payload) {
+    commit("set_title", payload);
   },
-  setInWidget ({ commit, dispatch }, payload) {
-    commit('set_in_widget', true)
+  setInWidget({ commit, dispatch }, payload) {
+    commit("set_in_widget", true);
   },
-  resetInWidget ({ commit, dispatch }) {
-    commit('set_in_widget', false)
+  resetInWidget({ commit, dispatch }) {
+    commit("set_in_widget", false);
   },
-  showAppbar ({ commit, dispatch }) {
-    commit('show_appbar')
+  showAppbar({ commit, dispatch }) {
+    commit("show_appbar");
   },
-  hideAppbar ({ commit, dispatch }) {
-    commit('hide_appbar')
+  hideAppbar({ commit, dispatch }) {
+    commit("hide_appbar");
   },
-  toggleDark ({ commit, getters }) {
-    const dark = !getters.dark
-    commit('set_dark', dark)
+  toggleDark({ commit, getters }) {
+    const dark = !getters.dark;
+    commit("set_dark", dark);
     return axiosInternal
-      .post('/setting/_general', { data: { dark } })
+      .post("/setting/_general", { data: { dark } })
       .then(() => {})
-      .catch(error => Promise.reject(error))
-  }
-}
+      .catch((error) => Promise.reject(error));
+  },
+};
 
 const mutations = {
-  set_contact (state, payload) {
-    state.contact = payload
+  set_contact(state, payload) {
+    state.contact = payload;
   },
-  set_menu (state, payload) {
-    state.menu = payload
+  set_menu(state, payload) {
+    state.menu = payload;
   },
-  set_version (state, payload) {
-    state.version = payload
+  set_version(state, payload) {
+    state.version = payload;
   },
-  set_in_widget (state, payload) {
-    state.inWidget = payload
+  set_in_widget(state, payload) {
+    state.inWidget = payload;
   },
-  set_dark (state, dark) {
+  set_dark(state, dark) {
     if (dark != null) {
-      getVuetify().framework.theme.dark = dark
-      state.dark = dark
+      getVuetify().framework.theme.dark = dark;
+      state.dark = dark;
     }
   },
-  set_profile (state, payload) {
-    state.profile = Object.assign({}, state.profile, payload)
+  set_profile(state, payload) {
+    state.profile = Object.assign({}, state.profile, payload);
   },
-  clear_profile (state, payload) {
+  clear_profile(state, payload) {
     state.profile = {
       authenticated: false,
-      name: null
-    }
+      name: null,
+    };
   },
-  set_loading (state, payload) {
-    state.loading = payload
+  set_loading(state, payload) {
+    state.loading = payload;
   },
-  show_appbar (state) {
-    state.appBarVisible = true
+  show_appbar(state) {
+    state.appBarVisible = true;
   },
-  hide_appbar (state) {
-    state.appBarVisible = false
+  hide_appbar(state) {
+    state.appBarVisible = false;
   },
-  set_title (state, payload) {
-    state.title = payload
-    document.title = payload
-    document.querySelector('body').classList.add(
-      payload
-        .toLowerCase()
-        .split(' ')
-        .join('-')
-    )
+  set_title(state, payload) {
+    state.title = payload;
+    document.title = payload;
+    document
+      .querySelector("body")
+      .classList.add(payload.toLowerCase().split(" ").join("-"));
   },
-  initialize_app (state, payload) {
-    state.title = payload.title
-  }
-}
+  initialize_app(state, payload) {
+    state.title = payload.title;
+  },
+};
 
 const getters = {
-  contact (state) {
-    return state.contact
+  contact(state) {
+    return state.contact;
   },
-  profile (state) {
-    return state.profile
+  profile(state) {
+    return state.profile;
   },
-  authenticated (state) {
-    return state.profile.authenticated
+  authenticated(state) {
+    return state.profile.authenticated;
   },
-  loading (state) {
-    return state.loading
+  loading(state) {
+    return state.loading;
   },
-  inWidget (state) {
-    return state.inWidget
+  inWidget(state) {
+    return state.inWidget;
   },
-  appBarVisible (state) {
-    return state.appBarVisible
+  appBarVisible(state) {
+    return state.appBarVisible;
   },
-  title (state) {
-    return state.title
+  title(state) {
+    return state.title;
   },
-  dark (state) {
-    return state.dark
+  dark(state) {
+    return state.dark;
   },
-  version (state) {
-    return state.version
+  version(state) {
+    return state.version;
   },
-  hasOwnTheme (state) {
-    return state.hasOwnTheme
+  hasOwnTheme(state) {
+    return state.hasOwnTheme;
   },
-  menu (state) {
-    const debug = process.env.NODE_ENV !== 'production'
-    const user = JSON.parse(window.localStorage.getItem('user'))
+  menu(state) {
+    const debug = process.env.NODE_ENV !== "production";
+    const user = JSON.parse(window.localStorage.getItem("user"));
     if (user == null) {
       return [
         {
-          path: '',
-          label: ''
-        }
-      ]
+          path: "",
+          label: "",
+        },
+      ];
     }
-    return (state.menu || []).map(item => {
-      const path = debug ? 'http://localhost:5001' : ''
-      const label = Object.keys(item)[0]
+    return (state.menu || []).map((item) => {
+      const path = debug ? "http://localhost:5001" : "";
+      const label = Object.keys(item)[0];
       return {
         path: `${path}${item[label]}?token=${user.token}`,
-        label
-      }
-    })
-  }
-}
+        label,
+      };
+    });
+  },
+};
 
 export default {
   state,
   actions,
   mutations,
-  getters
-}
+  getters,
+};
