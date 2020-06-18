d7443b6b8546755dbf559bf41adbfba8a4d8e3a6
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2020-06-18 13:18:51 +0200

    bump

--- core4/httpError.js
+++ core4/httpError.js
@@ -1,181 +1,217 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-function i18n (code, name='default') {
-  const defaultError = Vue.prototype.i18n.t('errors.default')
+function i18n(code, name = "default") {
+  const defaultError = Vue.prototype.i18n.t("errors.default");
 
   try {
-    const translate = Vue.prototype.i18n.t(`errors.${code}.${name}`)
-    return translate.includes('undefined') ? defaultError : translate
+    const translate = Vue.prototype.i18n.t(`errors.${code}.${name}`);
+    return translate.includes("undefined") ? defaultError : translate;
   } catch (e) {
-    return defaultError
+    return defaultError;
   }
 }
 
-function payload (err, actions, isClosableDialog=false, html) {
+function payload(err, actions, isClosableDialog = false, html) {
   let payload = {
     error: err,
     actions: actions,
-    close: isClosableDialog
-  }
+    close: isClosableDialog,
+  };
 
   if (err.json != null) {
-    payload.json = err.json
+    payload.json = err.json;
   } else {
-    payload.html = html || `${i18n()}`
+    payload.html = html || `${i18n()}`;
   }
 
-  return payload
+  return payload;
 }
 
 const actions = {
-  '502' () {
+  "502"() {
     return [
       {
         main: false,
-        name: Vue.prototype.i18n.t('reloadPage'),
-        action () {
-          Vue.prototype.$store.dispatch('reloadCurrentPage')
-        }
+        name: Vue.prototype.i18n.t("reloadPage"),
+        action() {
+          Vue.prototype.$store.dispatch("reloadCurrentPage");
+        },
       },
       {
         main: true,
-        name: Vue.prototype.i18n.t('toTheMainPage'),
-        action () {
-          Vue.prototype.$store.dispatch('gotoMainPage')
-        }
-      }
-    ]
+        name: Vue.prototype.i18n.t("toTheMainPage"),
+        action() {
+          Vue.prototype.$store.dispatch("gotoMainPage");
+        },
+      },
+    ];
   },
-  '400' () {
-    return []
+  "400"() {
+    return [];
   },
-  '403' () {
+  "403"() {
     return [
       {
         main: false,
-        name: Vue.prototype.i18n.t('contact'),
-        action () {
-          window.location.href = `mailto:${Vue.prototype.$store.getters.contact}`
-        }
+        name: Vue.prototype.i18n.t("contact"),
+        action() {
+          window.location.href = `mailto:${Vue.prototype.$store.getters.contact}`;
+        },
       },
       {
         main: true,
-        name: Vue.prototype.i18n.t('toTheMainPage'),
-        action () {
-          Vue.prototype.$store.dispatch('gotoMainPage')
-        }
-      }
-    ]
+        name: Vue.prototype.i18n.t("toTheMainPage"),
+        action() {
+          Vue.prototype.$store.dispatch("gotoMainPage");
+        },
+      },
+    ];
   },
-  '409' () {
+  "404"() {
     return [
       {
-        main: false,
-        name: Vue.prototype.i18n.t('toTheMainPage'),
-        action () {
-          Vue.prototype.$store.dispatch('gotoMainPage')
-        }
+        main: true,
+        name: Vue.prototype.i18n.t("toTheMainPage"),
+        action() {
+          Vue.prototype.$store.dispatch("gotoMainPage");
+        },
       },
+    ];
+  },
+  "409"() {
+    return [
       {
         main: true,
-        name: Vue.prototype.i18n.t('reloadPage'),
-        action () {
-          Vue.prototype.$store.dispatch('reloadCurrentPage')
-        }
-      }
-  
-    ]
+        name: Vue.prototype.i18n.t("reloadPage"),
+        action() {
+          Vue.prototype.$store.dispatch("reloadCurrentPage");
+        },
+      },
+      {
+        main: false,
+        name: Vue.prototype.i18n.t("toTheMainPage"),
+        action() {
+          Vue.prototype.$store.dispatch("gotoMainPage");
+        },
+      },
+    ];
   },
-  'noInternet' () {
-    const action =  {
+  noInternet() {
+    const action = {
       main: true,
-      name: Vue.prototype.i18n.t('checkConnectivity'),
+      name: Vue.prototype.i18n.t("checkConnectivity"),
       timer: 0,
-      action () {
-        if (action.timer) clearTimeout(action.timer)
+      action() {
+        if (action.timer) clearTimeout(action.timer);
 
-        Vue.prototype.$store.dispatch('setLoading', true)
+        Vue.prototype.$store.dispatch("setLoading", true);
 
         action.timer = setTimeout(() => {
           if (navigator.onLine) {
-            Vue.prototype.$store.dispatch('hideError')
+            Vue.prototype.$store.dispatch("hideError");
           }
 
-          Vue.prototype.$store.dispatch('setLoading', false)
-        }, 2000)
-      }
+          Vue.prototype.$store.dispatch("setLoading", false);
+        }, 2000);
+      },
     };
-    return [action]
+    return [action];
   },
-  'default' () {
+  default() {
     return [
       {
         main: false,
-        name: Vue.prototype.i18n.t('contact'),
-        action () {
-          window.location.href = `mailto:${Vue.prototype.$store.getters.contact}`
-        }
+        name: Vue.prototype.i18n.t("contact"),
+        action() {
+          window.location.href = `mailto:${Vue.prototype.$store.getters.contact}`;
+        },
       },
       {
         main: true,
-        name: Vue.prototype.i18n.t('toTheMainPage'),
-        action () {
-          Vue.prototype.$store.dispatch('gotoMainPage')
-        }
-      }
-    ]
-  }
-}
+        name: Vue.prototype.i18n.t("toTheMainPage"),
+        action() {
+          Vue.prototype.$store.dispatch("gotoMainPage");
+        },
+      },
+    ];
+  },
+};
 
 export default {
-  show (err, vm, info) {
+  show(err, vm, info) {
     // err: error trace
     // vm: component in which error occurred
     // info: Vue specific error information such as lifecycle hooks, events etc.
 
     if (err.isAxiosError) {
       if (Vue.prototype.$store.getters.loading) {
-        Vue.prototype.$store.dispatch('setLoading', false)
+        Vue.prototype.$store.dispatch("setLoading", false);
       }
 
       if (err.response) {
-        const errorCode = err.response.status.toString()
+        const errorCode = err.response.status.toString();
 
-        switch (errorCode){
+        switch (errorCode) {
           case "503":
           // error will be handled by 502 case, because missed "break" instruction
           case "502":
-            Vue.prototype.$store.dispatch('showError', payload(err, actions[errorCode](), false, `${i18n(errorCode)}`))
-            break
+            Vue.prototype.$store.dispatch(
+              "showError",
+              payload(err, actions[errorCode](), false, `${i18n(errorCode)}`)
+            );
+            break;
           case "400":
-            Vue.prototype.$store.dispatch('showError', payload(err, actions[errorCode](), true, `${i18n(errorCode)}`))
-            break
+            Vue.prototype.$store.dispatch(
+              "showError",
+              payload(err, actions[errorCode](), true, `${i18n(errorCode)}`)
+            );
+            break;
           case "401":
-            Vue.prototype.$store.dispatch('gotoLogin')
-            break
+            Vue.prototype.$store.dispatch("gotoLogin");
+            break;
           case "403":
-            Vue.prototype.$store.dispatch('showError', payload(err, actions[errorCode](), true, `${i18n(errorCode)}`))
-            break
+            Vue.prototype.$store.dispatch(
+              "showError",
+              payload(err, actions[errorCode](), true, `${i18n(errorCode)}`)
+            );
+            break;
           case "404":
-            Vue.prototype.$store.dispatch('gotoNotFoundPage')
-            break
+            Vue.prototype.$store.dispatch("gotoNotFoundPage");
+            break;
           case "409":
-            Vue.prototype.$store.dispatch('showError', payload(err, actions[errorCode](), false, `${i18n(errorCode)}`))
-            break
+            Vue.prototype.$store.dispatch(
+              "showError",
+              payload(err, actions[errorCode](), false, `${i18n(errorCode)}`)
+            );
+            break;
           default:
             // cases: 4xx, 5xx, 500, 405, 406
             // mail = `<a href="mailto:${Vue.prototype.$store.getters.contact}">${Vue.prototype.$store.getters.contact}</a>`
-            Vue.prototype.$store.dispatch('showError', payload(err, actions['default']()))
+            Vue.prototype.$store.dispatch(
+              "showError",
+              payload(err, actions["default"]())
+            );
         }
       } else {
         if (!navigator.onLine) {
           // no internet connection
-          Vue.prototype.$store.dispatch('showError', payload(err, actions['noInternet'](), false, `${i18n('networkError', 'noInternet')}`))
+          Vue.prototype.$store.dispatch(
+            "showError",
+            payload(
+              err,
+              actions["noInternet"](),
+              false,
+              `${i18n("networkError", "noInternet")}`
+            )
+          );
         } else {
           // all of possible not xhr errors
-          Vue.prototype.$store.dispatch('showError', payload(err, actions['default'](), true, `${i18n('networkError')}`))
+          Vue.prototype.$store.dispatch(
+            "showError",
+            payload(err, actions["default"](), true, `${i18n("networkError")}`)
+          );
         }
       }
     }
-  }
-}
+  },
+};
--- core4/translations/index.js
+++ core4/translations/index.js
@@ -1,122 +1,109 @@
-import VueI18n from 'vue-i18n'
-import Vue from 'vue'
+import VueI18n from "vue-i18n";
+import Vue from "vue";
 
-Vue.use(VueI18n)
+Vue.use(VueI18n);
 
 export const veeValidateDictionary = {
   de: {
     messages: {
       auth: `Bitte überprüfen Sie ihren Namen und das Passwort.`,
       passwordscore: `Das gewählte Passwort ist nicht sicher genug.`,
-      confirmspecial: `Die Passwörter stimmen nicht überein.`
-    }
+      confirmspecial: `Die Passwörter stimmen nicht überein.`,
+    },
   },
   en: {
     messages: {
       auth: `Please check your name and password.`,
       passwordscore: `The chosen password is not secure enough.`,
-      confirmspecial: `The passwords do not match.`
-    }
-  }
-}
+      confirmspecial: `The passwords do not match.`,
+    },
+  },
+};
 
 export const i18n = new VueI18n({
-  locale: 'en',
-  fallbackLocale: 'en',
+  locale: "en",
+  fallbackLocale: "en",
   messages: {
     en: {
-/*       'username': 'Username',
-      'password': 'Password',
-      'backToLogin': 'Back to Login',
-      'yourEmailAdress': 'Email Address',
-      'checkEmail': 'Please check your email inbox.',
-      'resetPassword': 'Reset password',
-      'resetPasswordSuccess': 'The password has been reset.',
-      'newPassword': 'New password',
-      'repeatPassword': 'Repeat password',
-      'passwordSecurity': 'Password security',
-      'requestNewPassword': 'Request', */
-      'reloadPage': 'Reload page',
-      'toTheMainPage': 'To the main page',
-      'checkConnectivity': 'Check connectivity',
-      'contact': 'Contact',
-      'error': 'Error',
- /*      'pw': {
-        '0': 'Very weak',
-        '1': 'Weak',
-        '2': 'Ok',
-        '3': 'Good',
-        '4': 'Great'
-      }, */
+      reloadPage: "Reload page",
+      toTheMainPage: "To the main page",
+      checkConnectivity: "Check connectivity",
+      contact: "Contact",
+      error: "Error",
       errors: {
-        'default': 'Contact us. Something is wrong in an unexpected place.<br>Get in touch with us as this isn\'t likely to get better on its own.',
-        '502': {
-          'default': 'Try Again Later.<br>Something has gone wrong, but it\'s likely to work again soon.<br>Check back in a while, and if it\'s still not working contact support.'
+        default:
+          "Contact us. Something is wrong in an unexpected place.<br>Get in touch with us as this isn't likely to get better on its own.",
+        "502": {
+          default:
+            "Try Again Later.<br>Something has gone wrong, but it's likely to work again soon.<br>Check back in a while, and if it's still not working contact support.",
         },
-        '403': {
-          'default': 'You are not allowed to access this resource.<br>Contact us if you need access to this resource.'
+        "403": {
+          default:
+            "You are not allowed to access this resource.<br>Contact us if you need access to this resource.",
         },
-        '409': {
-          'default': 'Version mismatch. Concurrent modification. <br>Hint: somebody edited your open document.'
+        "404": {
+          default:
+            "One or more elements you tried to access where not found.",
         },
-        '400': {
-          'default': 'Bad request. <br>Please take a look technical details.'
+        "409": {
+          default:
+            "Version mismatch. Concurrent modification. <br>Hint: somebody edited your open document.",
         },
-        'networkError': {
-          'noInternet': 'No internet connection. <br>Please check your connectivity and retry.',
-          'default': 'Network error. <br>Please try again later or contact us.',
+        "400": {
+          default: "Bad request. <br>Please take a look technical details.",
         },
-        'is500Error': 'A server error has occurred.<br>Please contact us at:',
-        'isAuthError': 'Please log in.',
-        'isPermissionsError': 'You are not authorized to access this resource. <br>Please contact us at:'
-      }
+        networkError: {
+          noInternet:
+            "No internet connection. <br>Please check your connectivity and retry.",
+          default: "Network error. <br>Please try again later or contact us.",
+        },
+        is500Error: "A server error has occurred.<br>Please contact us at:",
+        isAuthError: "Please log in.",
+        isPermissionsError:
+          "You are not authorized to access this resource. <br>Please contact us at:",
+      },
     },
     de: {
-/*       'username': 'Name',
-      'password': 'Passwort',
-      'backToLogin': 'Zurück zum Login',
-      'yourEmailAdress': 'E-Mail Adresse',
-      'checkEmail': 'Bitte überprüfen sie ihren Email-Posteingang.',
-      'resetPassword': 'Reset password',
-      'resetPasswordSuccess': 'Das passwort wurde zurückgesetzt.',
-      'newPassword': 'Neues Passwort',
-      'repeatPassword': 'Passwort wiederholen',
-      'passwordSecurity': 'Passwortsicherheit',
-      'requestNewPassword': 'Anfordern', */
-      'reloadPage': 'Seite neuladen',
-      'toTheMainPage': 'Zur Hauptseite',
-      'checkConnectivity': 'Überprüfen Sie die Konnektivität',
-      'contact': 'Kontakt',
-      'error': 'Error',
- /*      'pw': {
-        '0': 'Sehr schwach',
-        '1': 'Schwach',
-        '2': 'Ok',
-        '3': 'Gut',
-        '4': 'Großartig'
-      }, */
+      reloadPage: "Seite neuladen",
+      toTheMainPage: "Zur Hauptseite",
+      checkConnectivity: "Überprüfen Sie die Konnektivität",
+      contact: "Kontakt",
+      error: "Error",
       errors: {
-        'default': 'Kontaktiere uns. An einem unerwarteten Ort stimmt etwas nicht. <br>Nehmen Sie Kontakt mit uns auf, da dies allein wahrscheinlich nicht besser wird.',
-        '502': {
-          'default': 'Versuchen Sie es später noch einmal. <br>Es ist ein Fehler aufgetreten, aber es wird wahrscheinlich bald wieder funktionieren. <br>Versuchen Sie es in einer Weile erneut, und wenn es immer noch nicht funktioniert, wenden Sie sich an den Support.'
+        default:
+          "Kontaktiere uns. An einem unerwarteten Ort stimmt etwas nicht. <br>Nehmen Sie Kontakt mit uns auf, da dies allein wahrscheinlich nicht besser wird.",
+        "502": {
+          default:
+            "Versuchen Sie es später noch einmal. <br>Es ist ein Fehler aufgetreten, aber es wird wahrscheinlich bald wieder funktionieren. <br>Versuchen Sie es in einer Weile erneut, und wenn es immer noch nicht funktioniert, wenden Sie sich an den Support.",
         },
-        '403': {
-          'default': 'Sie dürfen nicht auf diese Ressource zugreifen. <br>Kontaktieren Sie uns, wenn Sie Zugriff auf diese Ressource benötigen.'
+        "403": {
+          default:
+            "Sie dürfen nicht auf diese Ressource zugreifen. <br>Kontaktieren Sie uns, wenn Sie Zugriff auf diese Ressource benötigen.",
         },
-        '409': {
-          'default': 'Versionskonflikt. Gleichzeitige Änderung. <br>Hinweis: Jemand hat Ihr geöffnetes Dokument bearbeitet.'
+        "404": {
+          default:
+            "One or more elements you tried to access where not found.",
         },
-        '400': {
-          'default': 'Ungültige Anforderung. <br>Bitte schauen Sie sich die technischen Details an.'
+        "409": {
+          default:
+            "Versionskonflikt. Gleichzeitige Änderung. <br>Hinweis: Jemand hat Ihr geöffnetes Dokument bearbeitet.",
         },
-        'networkError': {
-          'noInternet': 'Keine Internetverbindung. <br>Bitte überprüfen Sie Ihre Konnektivität und versuchen Sie es erneut.',
-          'default': 'Netzwerkfehler. <br>Bitte versuchen Sie es später noch einmal oder kontaktieren Sie uns.',
+        "400": {
+          default:
+            "Ungültige Anforderung. <br>Bitte schauen Sie sich die technischen Details an.",
         },
-        'is500Error': 'Ein Server Fehler ist aufgetreten.<br>Bitte kontaktieren sie uns unter:',
-        'isAuthError': 'Bitte loggen sie sich ein.',
-        'isPermissionsError': 'Sie sind nicht authorisiert, diese Ressource aufzurufen.<br>Bitte kontaktieren sie uns unter:'
-      }
-    }
-  }
-})
+        networkError: {
+          noInternet:
+            "Keine Internetverbindung. <br>Bitte überprüfen Sie Ihre Konnektivität und versuchen Sie es erneut.",
+          default:
+            "Netzwerkfehler. <br>Bitte versuchen Sie es später noch einmal oder kontaktieren Sie uns.",
+        },
+        is500Error:
+          "Ein Server Fehler ist aufgetreten.<br>Bitte kontaktieren sie uns unter:",
+        isAuthError: "Bitte loggen sie sich ein.",
+        isPermissionsError:
+          "Sie sind nicht authorisiert, diese Ressource aufzurufen.<br>Bitte kontaktieren sie uns unter:",
+      },
+    },
+  },
+});
