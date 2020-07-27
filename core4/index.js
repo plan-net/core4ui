6bc768dea66edb688efd1e5f1129ecac8d537643
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2020-07-27 08:48:28 +0200

    refctor naming c4datatable

--- core4/index.js
+++ core4/index.js
@@ -4,6 +4,7 @@ import C4Card from './components/c4-card/C4Card'
 import C4Dialog from './components/c4-dialog/C4Dialog'
 import C4Numbers from './components/c4-numbers/C4Numbers'
 import C4Notification from './components/c4-notification/C4Notification'
+import C4Datatable from './components/c4-datatable/C4Datatable'
 import { setRoutes } from './internal/routes/index.js'
 import { setAjaxConfig } from './internal/axios.config.js'
 // import { setCookieConfig } from './internal/cookie.service.js'
@@ -68,6 +69,7 @@ const install = (Vue, options) => {
   setAjaxConfig(options.config)
   // setCookieConfig(options.config.API)
 
+  Vue.component('c4-datatable', C4Datatable)
   Vue.component('c4-dialog', C4Dialog)
   Vue.component('c4-notification', C4Notification)
   Vue.component('c4-numbers', C4Numbers)
