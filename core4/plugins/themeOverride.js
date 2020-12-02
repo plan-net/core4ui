872f9303fe9105438931bc8b821389e9f2531218
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2020-12-02 14:19:49 +0100

    bump

--- core4/plugins/themeOverride.js
+++ core4/plugins/themeOverride.js
@@ -2,10 +2,13 @@ import { axiosInternal } from 'core4ui/core4/internal/axios.config.js'
 import _ from 'lodash'
 
 export function applyTheme (conf) {
+  this.$Loading.config(conf)
   axiosInternal.interceptors.response.use(
     response => {
+      console.log(response)
       try {
         const tester = response.config.baseURL + response.config.url
+        console.log(tester)
         if (tester.includes('core4/api/v1/store')) {
           // custom theme
           response.data.data.doc.theme = _.merge(response.data.data.doc.theme, conf.theme)
