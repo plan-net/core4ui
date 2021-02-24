c103860a2dd4801f5e1fbdf821f5f6280deef379
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2021-02-24 15:13:13 +0100

    bump

--- core4/components/c4-webapp/c4-navigation/C4Navigation.vue
+++ core4/components/c4-webapp/c4-navigation/C4Navigation.vue
@@ -8,9 +8,19 @@
     class="c4-navigation px-2"
   >
     <!-- @slot Use this slot for SideNavigation v-list -->
-    <v-row column no-gutters>
-      <v-col class="mt-12 pt-8 mb-12" cols="12" align="center">
-        <router-link to="/" class="home-link">
+    <v-row
+      column
+      no-gutters class="c4-navigation-logo-container"
+    >
+      <v-col
+        class="mt-12 pt-8 mb-12"
+        cols="12"
+        align="center"
+      >
+        <router-link
+          to="/"
+          class="home-link"
+        >
           <img
             style="max-height: 80px; min-height: 80px; max-width: 270px;"
             :src="$store.getters.logo"
@@ -21,7 +31,7 @@
     </v-row>
     <slot></slot>
 
-    <footer
+    <!--   <footer
       :class="textColorClass"
       class="px-8 pt-7 pb-3"
       :style="{ 'background-color': this.$vuetify.theme.themes.light.accent }"
@@ -49,7 +59,50 @@
       <v-row no-gutters>
         <p>© {{ new Date().getFullYear() }} Serviceplan Group</p>
       </v-row>
-    </footer>
+    </footer> -->
+    <v-footer
+      dark
+      padless
+      class="c4-navigation-footer"
+    >
+      <v-card
+        class="flex"
+        flat
+        tile
+        :class="textColorClass"
+        :style="{ 'background-color': this.$vuetify.theme.themes.light.accent }"
+      >
+        <v-card-title>
+          <v-row
+            no-gutters
+            class="pb-5"
+          >
+            <v-col
+              v-for="item in menu"
+              :key="item.label"
+            >
+              <v-btn
+                :dark="!isAccentLightColor"
+                v-if="item.type === 'mail'"
+                :href="item.path"
+                text
+              >
+                {{ item.label }}</v-btn>
+              <v-btn
+                :dark="!isAccentLightColor"
+                v-else
+                :to="{ name: 'content', params: { type: item.label } }"
+                text
+              >
+                {{ item.label }}</v-btn>
+            </v-col>
+          </v-row>
+        </v-card-title>
+        <v-card-text class="py-2 white--text text-center">
+          © {{ new Date().getFullYear() }} <strong>Serviceplan Group</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-navigation-drawer>
 </template>
 
